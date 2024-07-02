99f68f1dcb2c4adffc3bc79beddec04d7a204153
mohdzaid2411 <zaidjalees2015@gmail.com>
2024-07-03 00:36:37 +0530

    initial commit

---
+++ notes.js
@@ -0,0 +1,14 @@
+// npm init -y
+// npm i express
+// npm i nodemon --sava-dev
+//Inside package.json 
+/* 
+"scripts": {
+    "start":"nodemon index.js"
+  },
+*/
+//now u can run this command in terminal--->"npm run start"
+//npm i ejs 
+//npm i dotenv
+//npm i fast-two-sms
+//
